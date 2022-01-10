cec7c34c031bbe35009320cce659d611a2ecc709
彭伟 <pengweiqhca@sina.com>
2022-01-10 11:07:37 +0800

    Use Timer call CleanOldLogs

--- src/XxlJob.Core/Logger/JobLogger.cs
+++ src/XxlJob.Core/Logger/JobLogger.cs
@@ -9,17 +9,25 @@ using XxlJob.Core.Model;
 
 namespace XxlJob.Core.Logger;
 
-public class JobLogger : IJobLogger
+public class JobLogger : IJobLogger, IDisposable
+#if NETSTANDARD
+    , IAsyncDisposable
+#endif
 {
     private readonly ILogger<JobLogger> _logger;
 
     private readonly AsyncLocal<string> _logFileName = new();
 
     private readonly XxlJobOptions _options;
+
+    private readonly Timer _timer;
+
     public JobLogger(IOptions<XxlJobOptions> optionsAccessor, ILogger<JobLogger> logger)
     {
         _logger = logger;
         _options = optionsAccessor.Value;
+
+        _timer = new Timer(CleanOldLogs, null, 30 * 60 * 1000, 24 * 60 * 60 * 1000);
     }
 
     public void SetLogFile(long logTime, long logId)
@@ -27,12 +35,11 @@ public class JobLogger : IJobLogger
         try
         {
             var filePath = MakeLogFileName(logTime, logId);
+
             var dir = Path.GetDirectoryName(filePath);
             if (dir != null && !Directory.Exists(dir))
-            {
                 Directory.CreateDirectory(dir);
-                CleanOldLogs();
-            }
+
             _logFileName.Value = filePath;
         }
         catch (Exception ex)
@@ -42,30 +49,23 @@ public class JobLogger : IJobLogger
     }
 
     [MethodImpl(MethodImplOptions.NoInlining)]
-    public void Log(string pattern, params object?[] format)
-    {
-        var appendLog = format.Length == 0 ? pattern : string.Format(pattern, format);
-
-        LogDetail(GetLogFileName(), (EnhancedStackFrame)EnhancedStackTrace.Current().GetFrame(0), appendLog);
-    }
+    public void Log(string pattern, params object?[] format) =>
+        LogDetail(GetLogFileName(), (EnhancedStackFrame)EnhancedStackTrace.Current().GetFrame(0),
+            format.Length == 0 ? pattern : string.Format(pattern, format));
 
     [MethodImpl(MethodImplOptions.NoInlining)]
-    public void LogError(Exception ex)
-    {
-        LogDetail(GetLogFileName(), (EnhancedStackFrame)EnhancedStackTrace.Current().GetFrame(0), ex.Message + ex.StackTrace);
-    }
+    public void LogError(Exception ex) =>
+        LogDetail(GetLogFileName(), (EnhancedStackFrame)EnhancedStackTrace.Current().GetFrame(0), ex.ToStringDemystified());
 
     public LogResult ReadLog(long logTime, long logId, int fromLine)
     {
         var filePath = MakeLogFileName(logTime, logId);
+
         if (string.IsNullOrEmpty(filePath))
-        {
             return new LogResult(fromLine, 0, "readLog fail, logFile not found", true);
-        }
+
         if (!File.Exists(filePath))
-        {
             return new LogResult(fromLine, 0, "readLog fail, logFile not exists", true);
-        }
 
         // read file
         var logContentBuffer = new StringBuilder();
@@ -89,33 +89,25 @@ public class JobLogger : IJobLogger
         }
 
         // result
-        var logResult = new LogResult(fromLine, toLineNum, logContentBuffer.ToString(), false);
-        return logResult;
+        return new LogResult(fromLine, toLineNum, logContentBuffer.ToString(), false);
     }
 
     [MethodImpl(MethodImplOptions.NoInlining)]
-    public void LogSpecialFile(long logTime, long logId, string pattern, params object?[] format)
-    {
+    public void LogSpecialFile(long logTime, long logId, string pattern, params object?[] format) =>
         LogDetail(MakeLogFileName(logTime, logId), (EnhancedStackFrame)EnhancedStackTrace.Current().GetFrame(0), string.Format(pattern, format));
-    }
 
-    private string GetLogFileName()
-    {
-        return _logFileName.Value;
-    }
+    private string GetLogFileName() => _logFileName.Value;
 
-    private string MakeLogFileName(long logDateTime, long logId)
-    {
-        //log fileName like: logPath/HandlerLogs/yyyy-MM-dd/9999.log
-        return Path.Combine(_options.LogPath, Constants.HandleLogsDirectory,
+    //log fileName like: logPath/HandlerLogs/yyyy-MM-dd/9999.log
+    private string MakeLogFileName(long logDateTime, long logId) =>
+        Path.Combine(_options.LogPath, Constants.HandleLogsDirectory,
             DateTimeOffset.FromUnixTimeMilliseconds(logDateTime).ToString("yyyy-MM-dd"), $"{logId}.log");
-    }
 
     private void LogDetail(string logFileName, EnhancedStackFrame callInfo, string appendLog)
     {
         if (string.IsNullOrEmpty(logFileName)) return;
 
-        var formatAppendLog = $"{DateTime.Now:s} [{callInfo.MethodInfo.DeclaringType?.FullName}#{callInfo.MethodInfo.Name}]-[line {callInfo.GetFileLineNumber()}]-[thread {Thread.CurrentThread.ManagedThreadId}] {appendLog}{Environment.NewLine}";
+        var formatAppendLog = $"{DateTime.Now:s} [{callInfo.MethodInfo.DeclaringType?.FullName}#{callInfo.MethodInfo.Name}]-[line {callInfo.GetFileLineNumber()}]-[thread {Environment.CurrentManagedThreadId}] {appendLog}{Environment.NewLine}";
 
         try
         {
@@ -127,7 +119,7 @@ public class JobLogger : IJobLogger
         }
     }
 
-    private void CleanOldLogs()
+    private void CleanOldLogs(object state)
     {
         if (_options.LogRetentionDays <= 0)
         {
@@ -136,28 +128,27 @@ public class JobLogger : IJobLogger
 
         using var _ = ExecutionContext.SuppressFlow();
 
-        Task.Run(() =>
+        try
         {
-            try
-            {
-                var handlerLogsDir = new DirectoryInfo(Path.Combine(_options.LogPath, Constants.HandleLogsDirectory));
-                if (!handlerLogsDir.Exists)
-                {
-                    return;
-                }
+            var handlerLogsDir = new DirectoryInfo(Path.Combine(_options.LogPath, Constants.HandleLogsDirectory));
+            if (!handlerLogsDir.Exists) return;
 
-                var today = DateTime.UtcNow.Date;
-                foreach (var dir in handlerLogsDir.GetDirectories())
-                {
-                    if (DateTime.TryParse(dir.Name, out var dirDate) &&
-                        today.Subtract(dirDate.Date).Days > _options.LogRetentionDays)
-                        dir.Delete(true);
-                }
-            }
-            catch (Exception ex)
+            var today = DateTime.UtcNow.Date;
+            foreach (var dir in handlerLogsDir.GetDirectories())
             {
-                _logger.LogError(ex, "CleanOldLogs error.");
+                if (DateTime.TryParse(dir.Name, out var dirDate) &&
+                    today.Subtract(dirDate.Date).TotalDays > _options.LogRetentionDays)
+                    dir.Delete(true);
             }
-        });
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "CleanOldLogs error.");
+        }
     }
+
+    public void Dispose() => _timer.Dispose();
+#if NETSTANDARD
+    public ValueTask DisposeAsync() => _timer.DisposeAsync();
+#endif
 }
